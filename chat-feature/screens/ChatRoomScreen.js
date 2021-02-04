0679453f038fd984ee7eefc75e64c81390a6fec1
morganiverson <miverson@emich.edu>
2021-02-04 10:39:03 -0500

    Morgan - Added function skeleton to add and load messages from firestore.

--- chat-feature/screens/ChatRoomScreen.js
+++ chat-feature/screens/ChatRoomScreen.js
@@ -1,13 +1,31 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import {GiftedChat} from 'react-native-gifted-chat';
+
+import { View, StyleSheet, FlatList } from 'react-native';
+import { List, Divider } from 'react-native-paper';
+
+import firestore from '@react-native-firebase/firestore';
+
+const messagesFDB = 'MESSAGES';
 //https://amanhimself.dev/blog/chat-app-with-react-native-part-1/
 //https://amanhimself.dev/blog/chat-app-with-react-native-part-4/
 
 //ROOM SCREEN COMPONENT
 //STATES: MESSAGES
-export default function RoomScreen({navigation}) {
+export default function RoomScreen({navigation, SID}) {
+    //PULL MESSAGES FROM FIRESTORE
+        const storedMessages = firestore()
+        .collection(messagesFDB)
+        .doc(SID)
+        .get();
+        //MATCH CURRENT USER ID MATCH IS _id:1
+//        setMessages(storedMessages);
+              
     //MESSAGES
     const [messages, setMessages] = useState([
+        
+        //LOAD MESSAGES FROM SESSION
+        
         //MOCK MESSAGES
         
         //SYSTEM MESSAGE
@@ -39,7 +57,12 @@ export default function RoomScreen({navigation}) {
     
     //HELPER FUNCTION: ADDS NEW MESSAGE TO PREVIOUS MESSAGES AND POSTS
     function handleSend(newMessage = []){
-        setMessages(GiftedChat.append(messages,newMessage));
+        //SEND MESSAGES TO FIRESTORE
+//        newMessage.sid = ''; //SESSION ID
+//        newMessage.uid = ''; //USER ID
+//        firestore().collection(messagesFDB).add(newMessage);
+        
+        setMessages(GiftedChat.append(messages, newMessage));
     }
     
     return (
