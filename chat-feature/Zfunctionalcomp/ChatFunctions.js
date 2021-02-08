cfa4fad467e326f3362f51d69bf381378a385ffa
morganiverson <miverson@emich.edu>
2021-02-07 22:33:51 -0500

    Packages needed file updated.

--- chat-feature/Zfunctionalcomp/ChatFunctions.js
+++ chat-feature/Zfunctionalcomp/ChatFunctions.js
@@ -5,9 +5,7 @@ import 'firebase/firestore';
 import { useCollectionData } from 'react-firebase-hooks/firestore'
 
 const SESSIONSFDB = 'SESSIONS';
-const CURRENTSESSION = storedMessages = firestore()
-        .collection(messagesFDB)
-        .doc(SID);
+
 
 //PULL TOP X MESSAGES DOWN FROM FB SESSION
 function getMessages(SID) {
